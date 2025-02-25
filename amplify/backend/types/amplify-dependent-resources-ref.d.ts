543f52860cc17b5c9af7994931d27b505c61ec2f
Artem Staforkin <staforkinartem@yandex.ru>
2025-02-25 22:53:43 +0500

    amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "reactawsintegrationb8676af0": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "reacts3": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
